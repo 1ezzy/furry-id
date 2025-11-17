fb65a121b9a16257ba678f10ddb3b98fd406fe3b
1ezzy <ezra.furry@gmail.com>
2025-11-17 18:11:29 -0500

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { settings, ThemeInit } from 'svelte-ux';
+	import favicon from '$lib/assets/favicon.svg';
+
+	import './app.css';
+
+	let { children } = $props();
+
+	settings({
+		themes: {
+			light: ['light'],
+			dark: ['dark']
+		}
+	});
+</script>
+
+<ThemeInit />
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
