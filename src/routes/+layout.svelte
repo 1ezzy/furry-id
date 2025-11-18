7af4a42c0aab58f9f6b9d6f051e2d03efd66af41
1ezzy <ezra.furry@gmail.com>
2025-11-18 17:05:45 -0500

    basic shell for the app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { getSettings, settings, ThemeInit, ThemeSwitch } from 'svelte-ux';
+	import { Button, getSettings, settings, ThemeInit, ThemeSwitch } from 'svelte-ux';
 	import favicon from '$lib/assets/favicon.svg';
 
 	import './app.css';
@@ -23,8 +23,13 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<header class="flex h-16 items-center justify-end">
+<header class="bg-surface-300 flex h-16 items-center justify-end gap-4 px-8">
 	<ThemeSwitch theme={$currentTheme} />
+	<div class="h-1/2 w-px bg-black"></div>
+	<Button
+		size="lg"
+		icon={'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>'}
+	></Button>
 </header>
 <main
 	class="bg-surface-300 flex h-full min-h-screen flex-col p-4 md:min-h-[calc(100vh-4rem)] md:p-0"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,32 @@
 <script lang="ts">
+	import { Field, getSettings } from 'svelte-ux';
+	import PageShell from '$lib/components/PageShell.svelte';
+
+	const { currentTheme } = getSettings();
+
+	const title = 'Furry License Generator';
 </script>
 
-<h1>Furry License Generator</h1>
+<PageShell>
+	<h1 class="flex text-6xl">
+		{#each title.split('') as char}
+			<span
+				class="bg-linear-to-tr from-pink-400 to-blue-400 bg-clip-text px-px py-1 text-transparent"
+				style="-webkit-text-stroke: 1px {$currentTheme.dark ? 'white' : 'black'};"
+			>
+				{char === ' ' ? '\u00A0' : char}
+			</span>
+		{/each}
+	</h1>
+	<div class="flex h-full w-full rounded-2xl border border-black">
+		<div class="m-8 basis-3/5 self-center">
+			<div class="bg-surface-100 aspect-214/135 rounded-2xl"></div>
+		</div>
+		<div class="flex-1 basis-2/5">
+			<div class="m-8 grid grid-flow-col gap-4">
+				<Field label="First Name" value="Sean" />
+				<Field label="Last Name" value="Lynch" />
+			</div>
+		</div>
+	</div>
+</PageShell>
