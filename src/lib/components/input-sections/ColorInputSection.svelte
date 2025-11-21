6ddf7c506aa2f556ca48eb7887909e8856a67ff4
1ezzy <ezra.furry@gmail.com>
2025-11-21 16:22:00 -0500

    theme for color picker + separate colors

--- src/lib/components/input-sections/ColorInputSection.svelte
+++ src/lib/components/input-sections/ColorInputSection.svelte
@@ -6,16 +6,28 @@
 
 	let { section }: { section: { sectionHeader: string; fields: InputField[] } } = $props();
 
-	let hex = $state<string>('#000000');
+	let colors = $state<string[]>(section.fields.map(() => '#000000'));
 </script>
 
 <div class="flex flex-col gap-2">
 	<h3 class="text-primary text-2xl">{section.sectionHeader}</h3>
 	<div class="flex gap-2">
-		{#each section.fields as field}
+		{#each section.fields as field, index}
 			<Field class={basisClasses[field.basis]} label={field.label}>
-				<ColorPicker bind:hex position="responsive" />
+				<div class="theme">
+					<ColorPicker position="responsive" bind:hex={colors[index]} />
+				</div>
 			</Field>
 		{/each}
 	</div>
 </div>
+
+<style>
+	.theme {
+		--cp-bg-color: var(--color-surface-100);
+		--cp-border-color: var(--color-border);
+		--cp-text-color: var(--color-border);
+		--cp-input-color: var(--color-surface-300);
+		--cp-button-hover-color: var(--color-surface-200);
+	}
+</style>
